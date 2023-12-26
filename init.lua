c238b7cc53b1a3b27a2fe7648ca5793de650cf7c
Vladimir Sotskov <bruh@shitposting.expert>
2023-12-26 03:01:42 +0300

    remove unnecessary files, only init.lua from now on

--- init.lua
+++ init.lua
@@ -14,25 +14,17 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
     "lifepillar/vim-mucomplete",
-    'ishan9299/modus-theme-vim',
-	-- "nvim-treesitter/nvim-treesitter-textobjects",
-	"jose-elias-alvarez/null-ls.nvim",
 
-	-- Git related plugins
-	-- "tpope/vim-fugitive",
-	-- "tpope/vim-rhubarb",
 	"lewis6991/gitsigns.nvim",
 
 	"windwp/nvim-autopairs",
 	"lukas-reineke/indent-blankline.nvim", -- Add indentation guides even on blank lines
 	"numToStr/Comment.nvim", -- "gc" to comment visual regions/lines
 	"kyazdani42/nvim-web-devicons",
-	-- "norcalli/nvim-colorizer.lua",
     { 'echasnovski/mini.nvim', version = false },
 	"bluz71/vim-moonfly-colors",
     'projekt0n/github-nvim-theme',
     { 'kaarmu/typst.vim', ft = 'typst', lazy=false, },
-    "duane9/nvim-rg",
 	"Tetralux/odin.vim",
     "tikhomirov/vim-glsl",
     "haringsrob/nvim_context_vt",
@@ -149,10 +141,44 @@ set.inccommand = "split"
 vim.g.mapleader = " "
 vim.g.maplocalleader = '\\'
 
--- require("custom.treesitter")
--- require("custom.lsp")
-require("custom.mini")
+require('mini.align').setup()
+require('mini.surround').setup()
+require('mini.statusline').setup({
+  content = {
+    active = function()
+      local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+      local spell         = vim.wo.spell and (MiniStatusline.is_truncated(120) and 'S' or 'SPELL') or ''
+      local wrap          = vim.wo.wrap  and (MiniStatusline.is_truncated(120) and 'W' or 'WRAP')  or ''
+      local git           = MiniStatusline.section_git({ trunc_width = 75 })
+      local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+      local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+      local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+      local searchcount   = MiniStatusline.section_searchcount({ trunc_width = 75})
+      local location      = MiniStatusline.section_location({ trunc_width = 75 })
 
+      -- Usage of `MiniStatusline.combine_groups()` ensures highlighting and
+      -- correct padding with spaces between groups (accounts for 'missing'
+      -- sections, etc.)
+      return MiniStatusline.combine_groups({
+        { hl = mode_hl,                  strings = { mode, spell} },
+        { hl = 'MiniStatuslineDevinfo',  strings = { git, diagnostics } },
+        '%<', -- Mark general truncate point
+        { hl = 'MiniStatuslineFilename', strings = { filename } },
+        '%=', -- End left alignment
+        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+        { hl = mode_hl,                  strings = { searchcount, location } },
+      })
+    end,
+  },
+})
+require('mini.tabline').setup()
+require('mini.basics').setup({
+    options = {
+        basic = true,
+        extra_ui = true,
+        win_borders = 'double',
+    }
+})
 require("Comment").setup()
 
 require("gitsigns").setup()
